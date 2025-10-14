535bacbc2a81175588838e4796d1cc573d45d3ad
Abdullah <abdullahhaleem530@gmail.com>
2025-10-14 15:18:06 +0500

    Imported Uvicorn Library

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from database import engine
 from models import Base
 from curd import router as crud_router
 from fastapi.middleware.cors import CORSMiddleware
-
+import uvicorn
 app = FastAPI()
 
 # This is the new, more permissive CORS configuration
@@ -18,3 +18,5 @@ app.add_middleware(
 Base.metadata.create_all(bind=engine)
 
 app.include_router(crud_router, prefix="/todos", tags=["todos"])
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1",port=8000)
